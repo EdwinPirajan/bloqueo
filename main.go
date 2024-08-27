0d9feeadf0a3dffa3877d8bff2ba03a5a37d42f0
unknown <Administrador@CO0003CC3187.adc3.com.co>
2024-08-27 18:56:40 -0500

    sirva hp

--- main.go
+++ main.go
@@ -34,6 +34,8 @@ func main() {
 	}
 
 	fmt.Println("ScrapeBlocker v1.0.5")
+	fmt.Println("Checking for updates...")
+
 	systemManager := services.NewWindowsSystemManager()
 	err = systemManager.EnableDebugPrivilege()
 	if err != nil {
